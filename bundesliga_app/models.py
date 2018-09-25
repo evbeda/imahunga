09228d64e34f62543b87014be0ab51e99b172cbd
Zhinon <aranda.ariel.norberto@gmail.com>
2018-09-25 16:17:48 -0300

    add events almost done

--- bundesliga_app/models.py
+++ bundesliga_app/models.py
@@ -1,20 +1,16 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
 
-class Organizer(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.first_name
-
-
 class Event(models.Model):
-    event_id = models.IntegerField(primary_key=True)
+    event_id = models.CharField(max_length=200, primary_key=True)
     name = models.CharField(max_length=200)
-    organizer = models.ForeignKey(Organizer)
+    organizer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.name
--- bundesliga_site/views.py
+++ bundesliga_site/views.py
@@ -1,10 +1,15 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
+from django.shortcuts import render
 from social_django.models import UserSocialAuth
 from eventbrite import Eventbrite
+from bundesliga_app.models import Event
+from django.http import HttpResponseRedirect
 
 
 @method_decorator(login_required, name='dispatch')
@@ -15,23 +20,6 @@ class HomeView(TemplateView, LoginRequiredMixin):
 
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        # import ipdb; ipdb.set_trace()
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['user'] = self.request.user
-        eventbrite = Eventbrite(
-            get_auth_token(self.request.user),
-        )
-        context['me'] = eventbrite.get('/users/me/')
-        # Bring live and draf events of user
-        context['events'] = [
-            event
-            # Status : live, draft, canceled, started, ended, all
-            for event in eventbrite.get(
-                '/users/me/owned_events/?status=live,draft'
-            )['events']
-        ]
-        return context
 
 
 def get_auth_token(user):
@@ -48,3 +36,61 @@ def get_auth_token(user):
         print ('UserSocialAuth does not exists!')
     return token
 
+
+@method_decorator(login_required, name='dispatch')
+class SelectEvents(TemplateView, LoginRequiredMixin):
+
+    """ This is the index view. Here we display all the banners that the user
+    has created """
+
+    template_name = 'organizer/select_events.html'
+
+    def _get_event(self):
+        eventbrite = Eventbrite(
+            get_auth_token(self.request.user),
+        )
+        return [
+            event
+            # Status : live, draft, canceled, started, ended, all
+            for event in eventbrite.get(
+                '/users/me/owned_events/?status=live,draft'
+            )['events']
+        ]
+
+    def post(self, *args, **kwargs):
+        # import ipdb; ipdb.set_trace()
+        events = self._get_event()
+        request_body_elements = str(self.request.body).split('event_')
+        request_body_elements.pop(0)
+        events_id = []
+        # Put in array all the events id selected
+        for event_id in request_body_elements:
+            event = event_id.split('=')
+            events_id.append(event[0])
+
+        # For each event in the api, verify if the selected ones are corrects
+        for event_in_api in events:
+            # If the event id is an event of organizer
+            if event_in_api['id'] in events_id:
+                # Verify if this event already exist
+                if not Event.objects.filter(event_id=event_in_api['id']).exists():
+                    Event.objects.create(
+                        event_id=event_in_api['id'],
+                        name=event_in_api['name']['text'],
+                        organizer=self.request.user,
+                    )
+            else:
+                continue
+        return HttpResponseRedirect(reverse('index'))
+
+    def get_context_data(self, **kwargs):
+        # import ipdb; ipdb.set_trace()
+        context = super(SelectEvents, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+        eventbrite = Eventbrite(
+            get_auth_token(self.request.user),
+        )
+        context['me'] = eventbrite.get('/users/me/')
+        # Bring live and draf events of user
+        context['events'] = self._get_event()
+        return context
