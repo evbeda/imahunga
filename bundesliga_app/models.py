7cbb469a84377471f510a61b3a8cc0c6561e8809
biancasozzi <biancasozzi@gmail.com>
2018-09-25 11:05:22 -0300

    added event to models

--- bundesliga_app/models.py
+++ bundesliga_app/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Organizer(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.first_name
+
+
+class Event(models.Model):
+    event_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    organizer = models.ForeignKey(Organizer)
+
+    def __str__(self):
+        return self.name
