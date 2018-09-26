f07a407110e60d9feca9fd92b014e8c178db03fa
francob <betombino@gmail.com>
2018-09-26 11:59:50 -0300

    Event discount view created

--- bundesliga_app/urls.py
+++ bundesliga_app/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
-from . import views
+from .views import (
+    CreateDiscount,
+    HomeView,
+    SelectEvents,
+    EventDiscountsView,
+)
 
 urlpatterns = [
-    url(r'createDiscount$', views.CreateDiscount.as_view(), name="createDiscount"),
+    url(r'^$', HomeView.as_view(), name='index'),
+    url(r'^select_events/$', SelectEvents.as_view(), name='select_events'),
+    url(r'^events_discount/(?P<event_id>[0-9]+)/$', EventDiscountsView.as_view(), name='events_discount'),
+    url(r'^create_discount/(?P<event_id>[0-9]+)/$', CreateDiscount.as_view(), name='create_discount'),
 ]
