3446537cbc8a6b35308dab39b2a8e36e4da0c72d
francob <betombino@gmail.com>
2018-09-26 09:13:44 -0300

    Visualize save events

---
+++ bundesliga_app/utils.py
@@ -0,0 +1,17 @@
+""" This are the methods that supports the behaviour of the views """
+from social_django.models import UserSocialAuth
+
+
+def get_auth_token(user):
+
+    """
+    This method will receive a user and
+    return its repesctive social_auth token
+    """
+    try:
+        token = user.social_auth.get(
+            provider='eventbrite'
+        ).access_token
+    except UserSocialAuth.DoesNotExist:
+        print ('UserSocialAuth does not exists!')
+    return token
