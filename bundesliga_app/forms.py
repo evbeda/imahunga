e365c07573c3e4e156ede0dad75c3e3095d38eac
biancasozzi <biancasozzi@gmail.com>
2018-10-02 10:46:28 -0300

    modify event wip

---
+++ bundesliga_app/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
+CHOICES = (
+    ('fixed', 'Fixed Discount $'),
+    ('percentage', 'Percentage Discount %')
+)
+
+
+class DiscountForm(forms.Form):
+    discount_name = forms.CharField(max_length=200, required=True, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    discount_type = forms.ChoiceField(choices=CHOICES, widget=forms.Select(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    discount_value = forms.FloatField(required=True, widget=forms.NumberInput(
+        attrs={
+            'class': 'form-control row'
+        }
+    ))
+
+    def __init__(self, data=None, *args, **kwargs):
+        super(DiscountForm, self).__init__(data, *args, **kwargs)
+        if data and data.get('discount_type', CHOICES[0][1]) == CHOICES[0][0]:
+            self.fields['discount_value'].validators = [MinValueValidator(0)]
+        elif data and data.get('discount_type', CHOICES[1][1]) == CHOICES[1][0]:
+            self.fields['discount_value'].validators = [
+                MinValueValidator(0),
+                MaxValueValidator(100)
+            ]
+
+    def verify_selected_type(self, discount_type):
+        print("Selected Type")
+        if discount_type == CHOICES[0][0]:
+            self.fields['discount_type'] = discount_type
+        elif discount_type == CHOICES[1][0]:
+            self.fields['discount_type'] = discount_type
--- bundesliga_app/static/js/create_discount_logic.js
+++ bundesliga_app/static/js/create_discount_logic.js
@@ -1,26 +1,15 @@
 	$(document).ready(function(){
-			$('#percentage-value').hide()
-			$('#fixed-value').hide()
-			$('#insert_discount_title').hide()
-    		$('#discount_type').on('change',function() {
-      			if ( this.value == 'fixed'){
-      				$('#insert_discount_title').show()
-        			$('#fixed-value').show()
-					$('#percentage-value').hide()
-					$('#discount_percentage').val('')
-        		} else{
-        			if(this.value == 'percentage'){
-        				$('#insert_discount_title').show()
-        				$('#percentage-value').show()
-						$('#fixed-value').hide()
-						$('#discount_fixed').val('')
-        			}else{
-        				$('#percentage-value').hide()
-						$('#fixed-value').hide()
-						$('#discount_fixed').val('')
-						$('#insert_discount_title').hide()
-						$('#discount_percentage').val('')
-        			}
-        		}
-    		});
+		$('#fixed_symbol').hide()
+		$('#percentage_symbol').hide()
+		$('#id_discount_type').on('change',function() {
+			if (this.value == 'fixed'){
+				$('#fixed_symbol').show()
+				$('#percentage_symbol').hide()
+			} else{
+				if(this.value == 'percentage'){
+					$('#percentage_symbol').show()
+					$('#fixed_symbol').hide()
+				}
+			}
 		});
+	});
