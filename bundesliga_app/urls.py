181ec84344a69c40358599070a6ec1f436c11f18
francob <betombino@gmail.com>
2018-10-09 15:11:54 -0300

    Listing page event

--- bundesliga_app/urls.py
+++ bundesliga_app/urls.py
@@ -6,6 +6,7 @@ from .views import (
     EventDiscountsView,
     DeleteDiscountView,
     LandingPageBuyerView,
+    ListingPageEventView,
 )
 
 urlpatterns = [
@@ -16,4 +17,5 @@ urlpatterns = [
     url(r'^events_discount/(?P<event_id>[0-9]+)/(?P<discount_id>[0-9]+)/delete/$', DeleteDiscountView.as_view(), name='delete_discount'),
     url(r'^events_discount/(?P<event_id>[0-9]+)/(?P<discount_id>[0-9]+)/$', ManageDiscount.as_view(), name='modify_discount'),
     url(r'^landing_page/(?P<organizer_id>[0-9]+)/$', LandingPageBuyerView.as_view(), name='landing_page_buyer'),
+    url(r'^landing_page/(?P<organizer_id>[0-9]+)/event/(?P<event_id>[0-9]+)/$', ListingPageEventView.as_view(), name='listing_page_event'),
 ]
