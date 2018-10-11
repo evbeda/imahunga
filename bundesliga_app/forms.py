1de19a76724d80bccd2d7a8eb40e6cb8bf0e2d8d
Zhinon <aranda.ariel.norberto@gmail.com>
2018-10-11 11:19:58 -0300

    adding some empty states in index, select events, and manage discount

--- bundesliga_app/forms.py
+++ bundesliga_app/forms.py
@@ -9,16 +9,19 @@ CHOICES = (
 class DiscountForm(forms.Form):
     discount_name = forms.CharField(max_length=200, required=True, widget=forms.TextInput(
         attrs={
-            'class': 'form-control'
+            'class': 'form-control',
+            'placeholder': 'Insert a name code'
         }
     ))
     discount_value = forms.IntegerField(required=True, widget=forms.NumberInput(
         attrs={
-            'class': 'form-control'
+            'class': 'form-control',
+            'placeholder': 'Insert your discount'
         }),
         validators=[
             MinValueValidator(0),
             MaxValueValidator(100),
+
     ])
 
     def __init__(self, data=None, *args, **kwargs):
